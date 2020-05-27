80fd24a01f84300b0e8d22e2f236a5ddfc18a084
bernyfrancois <itoutoubernny@gmail.com>
2020-05-27 11:38:28 +0200

    configure ghpages deploy

---
+++ build/precache-manifest.dfe3e5583b1e049e6063a7f2ed28f6ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9681a9a814c7e2ac2163fff76b7c453e",
+    "url": "/ajc/index.html"
+  },
+  {
+    "revision": "885559bace6450eaefa7",
+    "url": "/ajc/static/css/2.4a00ddcd.chunk.css"
+  },
+  {
+    "revision": "5141bd25ab70684b7a18",
+    "url": "/ajc/static/css/main.c4ab03aa.chunk.css"
+  },
+  {
+    "revision": "885559bace6450eaefa7",
+    "url": "/ajc/static/js/2.0b711d0f.chunk.js"
+  },
+  {
+    "revision": "e95b3ebaec68caad50c330e63af5be13",
+    "url": "/ajc/static/js/2.0b711d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5141bd25ab70684b7a18",
+    "url": "/ajc/static/js/main.0fe22bbc.chunk.js"
+  },
+  {
+    "revision": "ce7b3f6dbac45ce89553",
+    "url": "/ajc/static/js/runtime-main.202cc64c.js"
+  },
+  {
+    "revision": "03723fc600a5a0573ebfdfb6d8627879",
+    "url": "/ajc/static/media/cdi.03723fc6.png"
+  },
+  {
+    "revision": "73d62034047bbb50fdb672474114ff71",
+    "url": "/ajc/static/media/logo.73d62034.png"
+  }
+]);
