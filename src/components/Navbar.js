54f029d95e803e640376096ac6ac0e26590d051b
bernyfrancois <itoutoubernny@gmail.com>
2020-05-27 13:40:20 +0200

    fix responsive issues on home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,59 @@
 import React from 'react';
 import logo from "../logo.png";
 import "./Navbar.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = ({mobileState, onClick}) => (
     <div className={`menus ${mobileState ? "mobile-active" : null}`}>
         <nav className="navbar navbar-expand-md">
-            <a className="navbar-brand" href="#home">
+            <a className="navbar-brand" data-menuanchor="home">
                 <img src={logo} alt="icon" className="brand__logo"/>
                 <span className="brand__name">Jeunesse Constructive</span>
             </a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul id="menu" className="navbar-nav">
-                    <li data-menuanchor="home" className="nav-item active">
-                        <a className="nav-link">Accueil</a>
+                    <li onClick={onClick} className="nav-item active">
+                        <NavLink to="/#home" className="nav-link">Accueil</NavLink>
                     </li>
-                    <li data-menuanchor="about" className="nav-item">
-                        <a className="nav-link">Présentation</a>
+                    <li onClick={onClick} className="nav-item">
+                        <NavLink to="/#about" className="nav-link">Présentation</NavLink>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href={"mooc241"}>Formation 241</a>
+                    <li onClick={onClick} className="nav-item">
+                        <a className="nav-link" href={"/mooc241"}>Formation 241</a>
                     </li>
-                    <li data-menuanchor="actus" className="nav-item">
-                        <a className="nav-link">Nos Actions</a>
+                    <li onClick={onClick} className="nav-item">
+                        <NavLink to={"/#actus"} className="nav-link">Nos Actions</NavLink>
                     </li>
-                    <li data-menuanchor="contact" className="nav-item">
-                        <a className="nav-link">Contact</a>
+                    <li onClick={onClick} className="nav-item">
+                        <NavLink to={"/#contact"} className="nav-link">Contact</NavLink>
                     </li>
                 </ul>
             </div>
         </nav>
-        <a onClick={onClick} className="toggle-btn">
+        <button onClick={onClick} className="toggle-btn">
             <span className="hamburger hamburger-1"/>
             <span className="hamburger hamburger-2"/>
             <span className="hamburger hamburger-3"/>
-        </a>
+        </button>
         <nav className="mobile-nav curtain">
             <span/>
             <span/>
             <span>
                 <ul id="mobile-menu" className="navbar-nav-mobile">
-                    <li onClick={onClick} data-menuanchor="home" className="nav-item active">
-                        <a className="nav-link">Accueil</a>
-                    </li>
-                    <li onClick={onClick} data-menuanchor="about" className="nav-item">
-                        <a className="nav-link">Présentation</a>
+                    <li onClick={onClick} className="nav-item active">
+                        <NavLink to="/#home" className="nav-link">Accueil</NavLink>
                     </li>
                     <li onClick={onClick} className="nav-item">
-                        <a className="nav-link" href={"mooc241"}>Formation 241</a>
+                        <NavLink to="/#about" className="nav-link">Présentation</NavLink>
                     </li>
-                    <li onClick={onClick} data-menuanchor="actus" className="nav-item">
-                        <a className="nav-link">Nos Actions</a>
+                    <li onClick={onClick} className="nav-item">
+                        <a className="nav-link" href={"/mooc241"}>Formation 241</a>
                     </li>
-                    <li onClick={onClick} data-menuanchor="contact" className="nav-item">
-                        <a className="nav-link">Contact</a>
+                    <li onClick={onClick} className="nav-item">
+                        <NavLink to={"/#actus"} className="nav-link">Nos Actions</NavLink>
+                    </li>
+                    <li onClick={onClick} className="nav-item">
+                        <NavLink to={"/#contact"} className="nav-link">Contact</NavLink>
                     </li>
                 </ul>
             </span>
