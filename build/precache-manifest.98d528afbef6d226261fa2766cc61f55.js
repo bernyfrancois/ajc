642b6990686c9eb0116066f3b663c077691ea245
bernyfrancois <itoutoubernny@gmail.com>
2020-06-05 12:49:04 +0200

    add somes new files

---
+++ build/precache-manifest.98d528afbef6d226261fa2766cc61f55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c668046472407b7ea7709376fb9791cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ac08081ed8185a8646b",
+    "url": "./static/css/2.4a00ddcd.chunk.css"
+  },
+  {
+    "revision": "161bf9b68dd2f1a91e72",
+    "url": "./static/css/main.09748071.chunk.css"
+  },
+  {
+    "revision": "2ac08081ed8185a8646b",
+    "url": "./static/js/2.71c6413e.chunk.js"
+  },
+  {
+    "revision": "e95b3ebaec68caad50c330e63af5be13",
+    "url": "./static/js/2.71c6413e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161bf9b68dd2f1a91e72",
+    "url": "./static/js/main.72e176bc.chunk.js"
+  },
+  {
+    "revision": "ddd56ce623145cbf6065",
+    "url": "./static/js/runtime-main.fce20a7c.js"
+  },
+  {
+    "revision": "03723fc600a5a0573ebfdfb6d8627879",
+    "url": "./static/media/cdi.03723fc6.png"
+  },
+  {
+    "revision": "73d62034047bbb50fdb672474114ff71",
+    "url": "./static/media/logo.73d62034.png"
+  }
+]);
